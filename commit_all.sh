5e4d577eedc42dcac780b6c4154dc9b1cb41ba84
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-10-07 12:13:27 -0300

    Fixing some tests

--- commit_all.sh
+++ commit_all.sh
@@ -8,17 +8,25 @@ fi
 COMMIT_MESSAGE="$1"
 
 # Commit and push changes in the submodule
-echo "Committing changes in submodule 'backend'..."
-cd backend
-git add .
-git commit -m "$COMMIT_MESSAGE (submodule)"
-git push origin main # Assuming 'main' is the branch for the submodule
-cd ..
+if [ -d "backend" ]; then
+  echo "Committing changes in submodule 'backend'..."
+  cd backend
+  git add .
+  git commit -m "$COMMIT_MESSAGE"
+  git push origin main # Assuming 'main' is the branch for the submodule
+  cd ..
+else
+  echo "Backend directory not found. Skipping submodule commit."
+fi
+
+# Update the submodule reference in the main repository
+echo "Updating submodule reference..."
+git submodule update --remote
 
 # Commit and push changes in the main repository
 echo "Committing changes in main repository..."
 git add .
 git commit -m "$COMMIT_MESSAGE"
-git push origin main # Assuming 'master' is the branch for the main repository
+git push origin main # Assuming 'main' is the branch for the main repository
 
 echo "Done."
